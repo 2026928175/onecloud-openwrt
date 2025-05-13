25a3d162de9a2f0ad7b4592e95b9c508d90df33d
2026928175 <2026928175@qq.com>
2025-05-13 17:18:00 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 #
-# OpenWrt DIY script part 1 (Before Update feeds)
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 可选：启用默认的部分 feed（根据你的 feeds.conf.default 而定）
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 sed -i '2s/^#//' feeds.conf.default
 sed -i '3s/^/#/' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-# 添加 luci-app-openclash（不走 feed，直接 clone）
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash --depth=1
-
-# 添加 luci-app-alist（Alist 网盘目录）
 git clone -b lua https://github.com/sbwml/luci-app-alist package/alist
-
-# 添加 luci-app-upnp（此插件已在 OpenWrt 官方 feeds 中，不需额外 feed）
-# 后续在 menuconfig 中选择 Network > UPnP Support > luci-app-upnp
+# git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
